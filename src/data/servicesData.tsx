9653343c4ed6745b53122e81508eaed22b66db6e
vijay1897 <vijayyuvi1897@gmail.com>
2023-05-27 15:57:51 +0530

    feat: update to the datafile

--- src/data/servicesData.tsx
+++ src/data/servicesData.tsx
@@ -1,42 +1,52 @@
-export const servicesData = [
+const servicesData = [
   {
+    key: 2,
     title: "Healthcare Provider",
     description:
       "Assistance for connecting qualified candidates with top companies",
   },
   {
+    key: 3,
     title: "Healthcare Provider",
     description:
       "Assistance for connecting qualified candidates with top companies",
   },
   {
+    key: 4,
     title: "Healthcare Provider",
     description:
       "Assistance for connecting qualified candidates with top companies",
   },
   {
+    key: 5,
     title: "Healthcare Provider",
     description:
       "Assistance for connecting qualified candidates with top companies",
   },
   {
+    key: 6,
     title: "Healthcare Provider",
     description:
       "Assistance for connecting qualified candidates with top companies",
   },
   {
+    key: 7,
     title: "Healthcare Provider",
     description:
       "Assistance for connecting qualified candidates with top companies",
   },
   {
+    key: 8,
     title: "Healthcare Provider",
     description:
       "Assistance for connecting qualified candidates with top companies",
   },
   {
+    key: 9,
     title: "Healthcare Provider",
     description:
       "Assistance for connecting qualified candidates with top companies",
   },
 ];
+
+export default servicesData;
